dceeee145975aecc0484a4eba356ec830e712d31
Nayan Gajera <nayan@saasinnova.com>
2026-01-06 15:49:40 +0530

    Add custom icon support to ITreeSelects interface.

--- TreeSelects.model.ts
+++ TreeSelects.model.ts
@@ -2,4 +2,6 @@ import { IFormProps } from "../formInterface/forms.model";
 
 export interface ITreeSelects extends IFormProps {
   showMarkAllHeader?: boolean;
+  isSetCustomIcon?: boolean;
+  customIcon?: JSX.Element | string;
 }
